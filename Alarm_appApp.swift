fee133577d094f8e09a980e8678053a32e208ae4
Berkekacar <kacarberke@icloud.com>
2024-08-31 18:20:49 +0300

    bug fix

--- Alarm_appApp.swift
+++ Alarm_appApp.swift
@@ -12,7 +12,7 @@ import SwiftData
 struct Alarm_appApp: App {
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
-            Item.self,
+            Alarm.self,
         ])
         let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
 
