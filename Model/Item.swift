50fa418e39d53b841cc3c3bf3b7eeaa8ae58580c
Berkekacar <kacarberke@icloud.com>
2024-08-31 18:10:20 +0300

    data model for alarms

---
+++ Model/Item.swift
@@ -0,0 +1,30 @@
+//
+//  Item.swift
+//  Alarm-app
+//
+//  Created by Berke Kaçar on 31.08.2024.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Alarm {
+    @Attribute(.unique) var id: UUID
+    var timestamp: Date
+    var isActive: Bool
+    var label: String
+    var repeatDays: [String] // Haftanın günlerini temsil eden dizi (örneğin, ["Monday", "Wednesday"])
+    var soundName: String
+    var isSnoozeEnabled: Bool
+    
+    init(timestamp: Date, isActive: Bool = true, label: String = "Alarm", repeatDays: [String] = [], soundName: String = "Default", isSnoozeEnabled: Bool = true) {
+        self.id = UUID()  // Her alarm için benzersiz bir kimlik
+        self.timestamp = timestamp
+        self.isActive = isActive
+        self.label = label
+        self.repeatDays = repeatDays
+        self.soundName = soundName
+        self.isSnoozeEnabled = isSnoozeEnabled
+    }
+}
